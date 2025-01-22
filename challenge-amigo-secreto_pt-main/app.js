bbbc6334b39e1e9f95991b56149591725f35dc05
cauasantoslt <cauasantos935@gmail.com>
2025-01-21 22:56:57 -0300

    First Commit

---
+++ challenge-amigo-secreto_pt-main/app.js
@@ -0,0 +1,19 @@
+let amigos = [];
+
+let amigo = document.getElementById('amigo');
+let botao = document.getElementById('botao');
+let resultado = document.getElementById('resultado');
+
+function adicionarAmigo() {
+  let amigoadd = amigo.value;
+  amigos.push(amigoadd);
+  resultado.textContent = "O amigo adicionado foi:" + amigos;
+}
+
+function adicionarAmigo() {
+  let amigoadd = amigo.value.trim();
+  if (amigoadd === '') {
+    alert('Por favor, insira um valor.');
+    return;
+  } 
+}
