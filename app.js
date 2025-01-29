fe22943f3af84679540080073bbdc8e0ac5bb53e
cauasantoslt <cauasantos935@gmail.com>
2025-01-28 21:24:01 -0300

    Confirmação através do enter adicionada

--- app.js
+++ app.js
@@ -6,6 +6,12 @@ let resultado = document.getElementById('resultado');
 function adicionarAmigo() {
   let novoAmigo = amigo.value.trim();
 
+  //Alerta se o campo estiver vazio
+  if(novoAmigo==='') {
+    alert('Preencha o campo com um nome!');
+    return;
+  }
+
   // Verifica se o nome já existe na lista
   if (amigos.includes(novoAmigo)) {
     alert('Este nome já foi adicionado.');
@@ -21,6 +27,23 @@ function adicionarAmigo() {
   // Limpa o campo de entrada
   amigo.value = '';
 }
+
+//Adiciona um novo amigo ao clicar enter 
+amigo.addEventListener('keyup', (event) => {
+  if (event.key === 'Enter') {
+    event.preventDefault();
+    const novoAmigo = amigo.value.trim();
+
+    if (novoAmigo === '') {
+      alert('Por favor, digite um nome válido.');
+    } else if (amigos.includes(novoAmigo)) {
+      alert('Este amigo já foi adicionado.');
+    } else {
+      adicionarAmigo();
+      amigo.focus(); // Move o foco para o campo de entrada
+    }
+  }
+});
 
 //Função para sortear os amigos
 function sortearAmigo() {
